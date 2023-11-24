50d99ff9c3dbb800be36fce5de6fe6d36311ac03
Michele Olivieri - michele.olivieri3@studio.unibo.it <michele.olivieri3@studio.unibo.it>
2023-11-24 11:42:32 +0100

    improve database.php

--- 01-lab/database.php
+++ 01-lab/database.php
@@ -15,8 +15,30 @@ class DatabaseHelper {
         $stmt->bind_param('i', $n);
         $stmt->execute();
         $result = $stmt->get_result();
-        
+
         return $result->fetch:all(MYSQLI_ASSOC);
+    }
+
+    public function getCategories(){
+        $stmt = $this->db->prepare("SELECT * FROM categoria");
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getPost($n){
+        $query = "SELECT idarticolo, titoloarticolo, imgarticolo, anteprimaarticolo, dataarticolo, nome FROM articolo, autore WHERE autore=idautore ORDER BY dataarticolo DESC";
+        if ($n > 0) $query = "LIMIT ?";
+
+        $stmt = $this->db->prepare($query);
+
+        if ($n < 0) $stmt->bind_param('i', $n);
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
 }
