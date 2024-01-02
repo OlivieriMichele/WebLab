4b9d1a4c9b473993a3ecb65b9615123ecac31fb4
Michele Olivieri <micheleolivieri02@gmail.com>
2024-01-02 17:47:38 +0100

    implement all of requirment

--- elaboratoPHP/michele.olivieri3.php
+++ elaboratoPHP/michele.olivieri3.php
@@ -29,7 +29,7 @@
 
     // Controllare che la variabile "O" non sia nulla e che sia uguale a "i" o "u".
     function validate($o){
-        return ($o === 'o' || $o === 'u') ? $o : null;
+        return ($o === 'i' || $o === 'u') ? $o : null;
     }
 
     if (isset($_GET['A']) && isset($_GET['B']) && isset($_GET['O'])) {
@@ -55,18 +55,38 @@
             while($row = $resultB->fetch_assoc()){
                 $insiemeB[] = $row['valore'];
             }
-            
-            for($i=0; $i<3; $i++){
-                echo "A[$i] = $insiemeA[$i] ";
-                echo "B[$i] = $insiemeB[$i] ";
-            }
 
             // Creare un nuovo vettore contenente l'unione dei due insiemi se O vale u, altrimenti dovrà contenere l'intersezione dei due insiemi.
+            $nuovoInsieme = [];
+            $nuovoInsieme = ($O === 'u') ? array_unique(array_merge($insiemeA,$insiemeB)) : (array_intersect($insiemeA,$insiemeB));
             
             // Inserire sul db il nuovo insieme, usando come id dell'insieme il successivo all'id massimo.
+            $maxIdQuery = "SELECT MAX(id) as max_id FROM insiemi";
+            $maxIdResult = $db->query($maxIdQuery);
+            $row = $maxIdResult->fetch_assoc();
+            $newId = $row['max_id'] + 1;
+
+            $numIndex = "SELECT MAX(insieme) as max_index FROM insiemi";
+            $maxIndexResult = $db->query($numIndex);
+            $row = $maxIndexResult->fetch_assoc();
+            $C = $row['max_index'] + 1;
+
+            foreach ($nuovoInsieme as $elem) {
+                $insertQuery = "INSERT INTO insiemi (id, valore, insieme) VALUES ($newId, '$elem', $C)";
+                $db->query($insertQuery);
+                $newId++;
+            }
+
+            /* test */
+            foreach($nuovoInsieme as $elem){
+                echo " $elem ";
+            }
 
         } else {
             echo "Error: A,B or O are not valid";
         }
+
+    } else {
+        echo "Error: some data is missing";
     }
 ?>
